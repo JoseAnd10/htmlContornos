6e8019a918b7c19db85dee430302d36b98ca5599
2024-25 DAM1 <you@example.comiagogarciaiglesias@gmail.com>
2024-10-21 17:55:49 +0200

    commit

---
+++ Documents/NetBeansProjects/repCMD/src/main/java/com/mycompany/repcmd/RepCMD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.repcmd;
+
+/**
+ *
+ * @author 2024-25 DAM1
+ */
+public class RepCMD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
