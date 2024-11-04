e93f1642956aead564ed8a616b8b6f1f7430b001
Gabriel D'león <gabrieldleonbarbeito@gmail.com>
2024-11-04 18:02:02 +0100

    creacion de clases

--- OperacionContorno/src/main/java/com/mycompany/operacioncontorno/OperacionContorno.java
+++ OperacionContorno/src/main/java/com/mycompany/operacioncontorno/OperacionContorno.java
@@ -3,6 +3,7 @@
  */
 
 package com.mycompany.operacioncontorno;
+import java.util.*;
 
 /**
  *
@@ -11,6 +12,30 @@ package com.mycompany.operacioncontorno;
 public class OperacionContorno {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bienvenido a la calculadora de Gabriel, Iago y José");
+        System.out.println("Para sumar escriba 1, para restar escriba 2 y para multiplicar escriba 3");
+        int seleccion;
+        seleccion= scanner.nextInt();
+        int continuar = 1;
+        while(continuar == 1){
+        switch(seleccion){
+            case 1: suma();
+            break;
+            case 2: resta();
+            break;
+            case 3: multiplicacion();
+            break;
+            default: System.out.println("Seleccione correctamente");
+            break;
+        }
+        System.out.println("Si desea ejecutar la calculadora nuevamente escriba 1");
+        continuar=scanner.nextInt();
+        }
     }
+    
+    public static void suma(){}
+    public static void resta(){}
+    public static void multiplicacion(){}
+    public static void division(){}
 }
