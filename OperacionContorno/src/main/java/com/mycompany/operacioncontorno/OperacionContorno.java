e0a575fbe565e0a1fa7d39b4a38dfa0410e8d9f5
Gabriel D'león <gabrieldleonbarbeito@gmail.com>
2024-11-04 17:46:29 +0100

    Versión inicial del proyecto

---
+++ OperacionContorno/src/main/java/com/mycompany/operacioncontorno/OperacionContorno.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operacioncontorno;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class OperacionContorno {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
