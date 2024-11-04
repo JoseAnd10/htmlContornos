f08fb76d1cbbeb3c5bc75c1991464a34f397ab9a
2024-25 DAM1 <you@example.comiagogarciaiglesias@gmail.com>
2024-11-04 16:55:05 +0100

    commit 1

--- Documents/Contornos/Operaciones/src/main/java/com/mycompany/operaciones/Operaciones.java
+++ Documents/Contornos/Operaciones/src/main/java/com/mycompany/operaciones/Operaciones.java
@@ -1,16 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.operaciones;
 
-/**
- * 
- * @author 2024-25 DAM1
- */
 public class Operaciones {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+    
 }
